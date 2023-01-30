9d4d0cc1eb00d4d646ec86855d00f03516eb7b0d
Victor <98951159+temptempest@users.noreply.github.com>
2023-01-31 02:41:58 +0400

    Add nil data and nil response to the request error test case to make the setup more explicit

--- EssentialAppTests/URLSessionHTTPClientTests.swift
+++ EssentialAppTests/URLSessionHTTPClientTests.swift
@@ -26,7 +26,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.startInterceptingRequest()
         let url = URL(string: "https://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
-        URLProtocolStub.stub(url: url, error: error)
+        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
         let sut = URLSessionHTTPClient()
         let exp = expectation(description: "Wait for completion")
         sut.get(from: url) { result in
@@ -47,12 +47,14 @@ class URLSessionHTTPClientTests: XCTestCase {
 // MARK: - Helpers
 extension URLSessionHTTPClientTests {
     private struct Stub {
+        let data: Data?
+        let response: URLResponse?
         let error: Error?
     }
     private class URLProtocolStub: URLProtocol {
         private static var stubs = [URL: Stub]()
-        static  func stub(url: URL, error: Error? = nil) {
-            stubs[url] = Stub(error: error )
+        static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
+            stubs[url] = Stub(data: data, response: response, error: error )
         }
         static func startInterceptingRequest() {
             URLProtocol.registerClass(URLProtocolStub.self)
@@ -71,6 +73,12 @@ extension URLSessionHTTPClientTests {
         override func startLoading() {
             guard let url = request.url,
                   let stub = URLProtocolStub.stubs[url] else { return }
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
             if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
             }
