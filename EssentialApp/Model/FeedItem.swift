7a2dad24ebe075de8544297d0d603c478c10d2b1
Victor <98951159+temptempest@users.noreply.github.com>
2023-01-30 17:35:32 +0400

    Create the FeedItem model

---
+++ EssentialApp/Model/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialApp
+//
+//  Created by temptempest on 30.01.2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+}
