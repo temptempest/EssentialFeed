a039d0bba5be30333fb61d21d91aaa8c71ffeeda
Victor <98951159+temptempest@users.noreply.github.com>
2023-01-30 22:12:14 +0400

    Delivers items array on 200 HTTP response with JSON items

--- EssentialApp/Util(Feed API)/RemoteFeedLoader.swift
+++ EssentialApp/Util(Feed API)/RemoteFeedLoader.swift
@@ -31,8 +31,8 @@ public struct RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success((data, _)):
-                if (try? JSONSerialization.jsonObject(with: data)) != nil {
-                    completion(.success([]))
+                if let root = try? JSONDecoder().decode(Root.self, from: data) {
+                    completion(.success(root.items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -41,4 +41,8 @@ public struct RemoteFeedLoader {
             }
         }
     }
+}
+
+private struct Root: Decodable {
+    let items: [FeedItem]
 }
